ff6cf130a47b98a61543ef07e29f1980c5fa8fa4
Ben Kazakevicius <benkazakev@gmail.com>
2026-03-29 00:31:27 +0000

    SD card reader added. updated UI

---
+++ include/SDLogger.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Arduino.h>
+#include "SensorManager.h"
+#include "UI.h"
+
+class SDLogger {
+public:
+    bool begin(uint8_t chipSelectPin);
+    bool log(unsigned long timestampMs, const SensorData& data, ExportFormat format);
+    bool isAvailable() const;
+
+private:
+    bool writeCsv(unsigned long timestampMs, const SensorData& data);
+    bool writeJson(unsigned long timestampMs, const SensorData& data);
+    bool fileExists(const char* filename) const;
+
+    uint8_t chipSelectPin = 0;
+    bool available = false;
+};
--- src/OLED.cpp
+++ src/OLED.cpp
@@ -1,52 +1,107 @@
 #include "OLED.h"
-#include <SPI.h>
+#include <stdio.h>
+#include <string.h>
 
 OLED::OLED(uint8_t dc, uint8_t reset, uint8_t cs)
-    : display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, dc, reset, cs) {}
+    : display(U8G2_R0, cs, dc, reset) {}
 
 bool OLED::begin() {
-    if (!display.begin(SSD1306_SWITCHCAPVCC)) {
-        return false;
-    }
-
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.display();
+    display.begin();
+    display.setFont(u8g2_font_5x8_tf);
+    clear();
     return true;
 }
 
 void OLED::clear() {
-    display.clearDisplay();
-    display.display();
+    commandCount = 0;
+    present();
 }
 
 void OLED::clearBuffer() {
-    display.clearDisplay();
+    commandCount = 0;
 }
 
 void OLED::present() {
-    display.display();
+    display.firstPage();
+    do {
+        for (uint8_t i = 0; i < commandCount; ++i) {
+            switch (commands[i].type) {
+            case CommandType::Text:
+                display.setCursor(commands[i].x, commands[i].y);
+                display.print(commands[i].text);
+                break;
+            case CommandType::Frame:
+                display.drawFrame(commands[i].x, commands[i].y, commands[i].width,
+                                  commands[i].height);
+                break;
+            case CommandType::Box:
+                display.drawBox(commands[i].x, commands[i].y, commands[i].width,
+                                commands[i].height);
+                break;
+            }
+        }
+    } while (display.nextPage());
 }
-// Prints text
+
 void OLED::drawText(int16_t x, int16_t y, const char* text) {
-    display.setCursor(x, y);
-    display.print(text);
+    addCommand(x, y, text);
 }
-// Prints readings with int value to screen
+
 void OLED::drawValueWithUnit(int16_t x, int16_t y, uint16_t value, const char* unit) {
-    display.setCursor(x, y);
-    display.print(value);
-    display.print(unit);
+    char buffer[MAX_TEXT_LENGTH];
+    snprintf(buffer, sizeof(buffer), "%u%s", value, unit);
+    addCommand(x, y, buffer);
 }
-// Prints readings with float value to screen
+
 void OLED::drawFloatWithUnit(int16_t x, int16_t y, float value, const char* unit,
                              uint8_t decimals, bool prependDegreeSymbol) {
-    display.setCursor(x, y);
-    display.print(value, decimals);
+    char valueBuffer[12];
+    dtostrf(value, 0, decimals, valueBuffer);
+
+    char buffer[MAX_TEXT_LENGTH];
     if (prependDegreeSymbol) {
-        display.write(247);
+        snprintf(buffer, sizeof(buffer), "%s%c%s", valueBuffer, 176, unit);
+    } else {
+        snprintf(buffer, sizeof(buffer), "%s%s", valueBuffer, unit);
     }
-    display.print(unit);
+
+    addCommand(x, y, buffer);
+}
+
+void OLED::drawFrame(int16_t x, int16_t y, uint8_t width, uint8_t height) {
+    addCommand(CommandType::Frame, x, y, width, height);
+}
+
+void OLED::drawBox(int16_t x, int16_t y, uint8_t width, uint8_t height) {
+    addCommand(CommandType::Box, x, y, width, height);
+}
+
+bool OLED::addCommand(int16_t x, int16_t y, const char* text) {
+    if (commandCount >= MAX_DRAW_COMMANDS) {
+        return false;
+    }
+
+    commands[commandCount].type = CommandType::Text;
+    commands[commandCount].x = x;
+    commands[commandCount].y = y;
+    strncpy(commands[commandCount].text, text, MAX_TEXT_LENGTH - 1);
+    commands[commandCount].text[MAX_TEXT_LENGTH - 1] = '\0';
+    ++commandCount;
+    return true;
+}
+
+bool OLED::addCommand(CommandType type, int16_t x, int16_t y, uint8_t width, uint8_t height) {
+    if (commandCount >= MAX_DRAW_COMMANDS) {
+        return false;
+    }
+
+    commands[commandCount].type = type;
+    commands[commandCount].x = x;
+    commands[commandCount].y = y;
+    commands[commandCount].width = width;
+    commands[commandCount].height = height;
+    commands[commandCount].text[0] = '\0';
+    ++commandCount;
+    return true;
 }
 
