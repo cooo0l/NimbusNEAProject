3b20a29d7d70f7de05748d2a840fa2c2c7045a86
Ben Kazakevicius <benkazakev@gmail.com>
2026-03-30 01:01:16 +0100

    Revert "UI added, with main menu"
    This reverts commit d1f3a45a55941abc6c126f3eaffa7c376fe0b75a.

--- include/OLED.h
+++ include/OLED.h
@@ -3,20 +3,16 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "SensorManager.h"
 
-// Encapsulates low-level display drawing for the OLED.
 class OLED {
 public:
     OLED(uint8_t dc, uint8_t reset, uint8_t cs);
 
     bool begin();
     void clear();
-    void clearBuffer();
-    void present();
-    void drawText(int16_t x, int16_t y, const char* text);
-    void drawValueWithUnit(int16_t x, int16_t y, uint16_t value, const char* unit);
-    void drawFloatWithUnit(int16_t x, int16_t y, float value, const char* unit,
-                           uint8_t decimals = 1, bool prependDegreeSymbol = false);
+    void showLoading();
+    void showSensorData(const SensorData& data);
 
 private:
     static const uint8_t SCREEN_WIDTH = 128;
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,23 @@
 #include <Arduino.h>
 #include "SensorManager.h"
 #include "OLED.h"
-#include "RotaryEncoder.h"
-#include "UI.h"
 
 namespace {
 constexpr uint8_t OLED_DC = 9;
 constexpr uint8_t OLED_CS = 10;
 constexpr uint8_t OLED_RESET = 8;
-constexpr uint8_t ENCODER_CLK = 2;
-constexpr uint8_t ENCODER_DT = 3;
-constexpr uint8_t ENCODER_SW = 4;
-constexpr unsigned long SENSOR_INTERVAL_MS = 1000;
 
 SensorManager sensors;
 OLED screen(OLED_DC, OLED_RESET, OLED_CS);
-RotaryEncoder encoder(ENCODER_CLK, ENCODER_DT, ENCODER_SW);
-UI ui(screen);
 
 unsigned long lastSensorUpdate = 0;
-
-void logSensorData(const SensorData& data) {
-    if (data.dhtValid) {
-        Serial.print("Temp: ");
-        Serial.print(data.temperature);
-        Serial.print(" C  Hum: ");
-        Serial.print(data.humidity);
-        Serial.println(" %");
-    } else {
-        Serial.println("DHT22 reading unavailable");
-    }
-
-    if (data.ccsValid) {
-        Serial.print("CO2: ");
-        Serial.print(data.CO2);
-        Serial.print(" ppm  TVOC: ");
-        Serial.print(data.TVOC);
-        Serial.println(" ppb");
-    } else {
-        Serial.println("CCS811 reading unavailable");
-    }
-}
+constexpr unsigned long SENSOR_INTERVAL_MS = 1000;
 }
 
 void setup() {
     Serial.begin(9600);
 
     sensors.begin();
-    encoder.begin();
 
     if (!screen.begin()) {
         Serial.println("ERROR: OLED screen failed to start");
@@ -55,25 +25,37 @@ void setup() {
         }
     }
 
-    ui.begin();
+    screen.showLoading();
 }
 
 void loop() {
-    const unsigned long now = millis();
-    encoder.update();
-
-    ui.moveSelection(encoder.getDelta());
-
-    if (encoder.wasPressed()) {
-        ui.select();
-    }
+    unsigned long now = millis();
 
     if (now - lastSensorUpdate >= SENSOR_INTERVAL_MS) {
         lastSensorUpdate = now;
         sensors.update();
 
-        const SensorData data = sensors.getData();
-        ui.updateSensorData(data);
-        logSensorData(data);
+        SensorData data = sensors.getData();
+        screen.showSensorData(data);
+
+        if (data.dhtValid) {
+            Serial.print("Temp: ");
+            Serial.print(data.temperature);
+            Serial.print(" C  Hum: ");
+            Serial.print(data.humidity);
+            Serial.println(" %");
+        } else {
+            Serial.println("DHT22 reading unavailable");
+        }
+
+        if (data.ccsValid) {
+            Serial.print("CO2: ");
+            Serial.print(data.CO2);
+            Serial.print(" ppm  TVOC: ");
+            Serial.print(data.TVOC);
+            Serial.println(" ppb");
+        } else {
+            Serial.println("CCS811 reading unavailable");
+        }
     }
 }
