1e234252d162947eed74f7a76b90571ed8415ee5
Ben Kazakevicius <benkazakev@gmail.com>
2026-05-08 13:06:30 +0100

    a

--- grapher/main.py
+++ grapher/main.py
@@ -2,12 +2,12 @@ import tkinter as tk
 
 from ui import GrapherApp
 
-
+# Create the root window
 def main() -> None:
     root = tk.Tk()
     GrapherApp(root)
     root.mainloop()
 
-
+# The entire program runs only when main.py is run
 if __name__ == "__main__":
     main()
